b41060764e94ff235bdc367cfdca91805d955ca1
Kee <codinginpain@gmail.com>
2023-04-16 22:31:15 +0900

    cookie 작업 및 로그인 유지

--- backend/api-server.js
+++ backend/api-server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const members = [
   {
@@ -19,15 +20,18 @@ const members = [
   },
 ];
 
+app.use(cookieParser());
 app.use(bodyParser.json());
 
+
 app.get('/api/account', (req, res) => {
-  res.send(401
-  //   {
-  //   mid: 3,
-  //   memberName: "홍길동",
-  // }
-  );
+  if(req.cookies && req.cookies.account) {
+    const member = JSON.parse(req.cookies.account);
+    if(member.id) {
+      return res.send(member);
+    }
+  }
+  res.send(401);
 });
 
 app.post('/api/account', (req, res) => {
@@ -38,6 +42,7 @@ app.post('/api/account', (req, res) => {
 
   const member = members.find(m => m.loginId === loginId && m.loginPw === loginPw);
   if(member) {
+    res.cookie("account", JSON.stringify(member));
     res.send(member);
   }
   res.send(404);
